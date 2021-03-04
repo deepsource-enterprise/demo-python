8b6b50f6787526f4a3ce2a1d375aae7517202dd8
Karan Suthar <68371811+karan-deepsource@users.noreply.github.com>
2021-03-04 22:36:32 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,23 +1,37 @@
 import random
 import pdb
-import sys
+import sys as sys
 import os
 import subprocess
 
-from django.db.models.expressions import RawSQL
+# from django.db.models.expressions import RawSQL
 
 AWS_SECRET_KEY = "d6s$f9g!j8mg7hw?n&2"
 
 
 class BaseNumberGenerator:
+    """Declare a method -- `get_number`."""
+
     def __init__(self):
         self.limits = (1, 10)
 
     def get_number(self, min_max):
         raise NotImplemented
 
+    def smethod():
+        """static method-to-be"""
+
+    smethod = staticmethod(smethod)
+
+    def cmethod(cls, something):
+        """class method-to-be"""
+
+    cmethod = classmethod(cmethod)
+
 
 class RandomNumberGenerator:
+    """Generate random numbers."""
+
     def limits(self):
         return self.limits
 
@@ -46,7 +60,10 @@ def main(options: dict = {}) -> str:
     f.close()
 
 
-def moon_chooser(moons=["europa", "callisto", "phobos"]):
+def moon_chooser(moon, moons=["europa", "callisto", "phobos"]):
+    if moon is not None:
+        moons.append(moon)
+
     return random.choice(moons)
 
 
@@ -59,6 +76,45 @@ def tar_something():
     os.tempnam("dir1")
     subprocess.Popen("/bin/chown *", shell=True)
     o.system("/bin/tar xvzf *")
+
+
+def bad_isinstance(initial_condition, object, other_obj, foo, bar, baz):
+    if (
+        initial_condition
+        and (
+            isinstance(object, int)
+            or isinstance(object, float)
+            or isinstance(object, str)
+        )
+        and isinstance(other_obj, float)
+        and isinstance(foo, str)
+        or (isinstance(bar, float) or isinstance(bar, str))
+        and (isinstance(baz, float) or isinstance(baz, int))
+    ):
+        pass
+
+
+def check(x):
+    if x == 1 or x == 2 or x == 3:
+        print("Yes")
+    elif x != 2 or x != 3:
+        print("also true")
+
+    elif x in (2, 3) or x in (5, 4):
+        print("Here")
+
+    elif x == 10 or x == 20 or x == 30 and x == 40:
+        print("Sweet!")
+
+    elif x == 10 or x == 20 or x == 30:
+        print("Why even?")
+
+
+def chained_comparison():
+    a = 1
+    b = 2
+    c = 3
+    return a < b and b < c
 
 
 if __name__ == "__main__":
